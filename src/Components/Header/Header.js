f498f139e49791e522e25ba16528816676b62531
Yushan Wang <yushanwang0816@gmail.com>
2023-04-28 09:46:10 +0800

    update project page

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import "./Header.css";
+import {useRef} from 'react';
 
 const Header = ({switchTheme, theme}) => {
+    const navRef = useRef();
+    // const [isOpen, setIsOpen] = useState(false);
+
+    const showNavbar = () => {
+        navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+    }
+
     return (
         <div className ="Header">
             <input id="nav-toggle" type="checkbox" /> 
@@ -9,12 +19,19 @@ const Header = ({switchTheme, theme}) => {
                 <div className="logo">
                     <a href="/"><h1>XIRUI HE</h1></a>   
                 </div>
-                <nav className="navbar">
-                    <a href="/#/project">Design</a>
-                    <a href="/#/playground">Playground</a>
-                    <a href="/#/connect">Connect</a>
+                <nav className="navbar" ref={navRef}>
+                    <a href="/#/project" 
+                    onClick={() => {showNavbar();}}>
+                        Design</a>
+                    <a href="/#/playground" 
+                    onClick={() => {showNavbar(); }}>
+                        Playground</a>
+                    <a href="/#/connect" 
+                    onClick={() => {showNavbar();}}>
+                        Connect</a>
                 </nav>
-                <label htmlFor="nav-toggle" className="menuBar">
+                <label htmlFor="nav-toggle" className="menuBar" 
+                    onClick={() => {showNavbar(); }}>
                         <div className="line"></div>
                         <div className="line"></div>
                         <div className="line"></div>
